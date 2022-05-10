5f0cf337667316450d42a78390f01c881eff0f4a
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-10 09:14:30 -0300

    Initial commit

---
+++ Assets/Instantiafor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiafor : MonoBehaviour
+{
+    public GameObject prefab;
+    
+
+    public void CloneObject()
+    {
+        GameObject clon;
+        for (int i=0;i<300;i++)
+        { 
+            clon = Instantiate(prefab);
+            clon.transform.Rotate(0, 45 * i, 0);
+            clon.transform.Translate(i*.3f,i,0);
+            
+            //Destroy(clon,.5f);
+        }
+        
+    }
+}
